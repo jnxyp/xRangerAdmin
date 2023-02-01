8ca541ec6e0a26352b53f381c057d78b035c07fc
wangxua <Xuan.Wang@pointclickcare.com>
2023-01-31 21:00:11 -0500

    remove index url path

--- xRangerAdmin/urls.py
+++ xRangerAdmin/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('adminManagement.urls')),
-    path('admin/', admin.site.urls),
+    path('adminManagement/', include('adminManagement.urls')),
+    path('', admin.site.urls),
 ]
 
 
